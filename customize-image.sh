aa50ff098ec937786d679ab0b088d28c8d73d59b
Nathan Henrie <nate@n8henrie.com>
2022-02-12 16:09:51 -0700

    Copy helpful files into subvolumes

--- customize-image.sh
+++ customize-image.sh
@@ -26,9 +26,6 @@ main() {
 
   mount -t btrfs "${part}" "${tmpmount}"
   btrfs filesystem resize max "${tmpmount}"
-  mkdir -p "${tmpmount}/root/.ssh"
-  cp /home/n8henrie/git/sd_shrink/nixpi_id_rsa.pub "${tmpmount}/root/.ssh/authorized_keys"
-  cp ./mountsubvols.sh ./make_swap.sh "${tmpmount}/root/"
 
   local subvols dest
   subvols=(@ @boot @home @nix @snapshots @swap @var)
@@ -36,6 +33,12 @@ main() {
     dest="${tmpmount}/${sv}"
     [[ -d "${dest}" ]] || btrfs subvol create "${dest}"
   done
+
+  for dir in nix boot; do
+    find "${tmpmount}/${dir}" -mindepth 1 -maxdepth 1 -exec mv -t "${tmpmount}/@${dir}" -- {} +
+    rm -r "${tmpmount:?}/${dir}"
+  done
+  mv "${tmpmount}/nix-path-registration" "${tmpmount}/@/"
   umount --recursive "${tmpmount}"
 
   for sv in @ @home @nix @var; do
@@ -55,7 +58,12 @@ main() {
   mkdir -p "${tmpmount}/.snapshots"
   mount -o compress-force=zstd,ssd_spread,subvol=@snapshots "${part}" "${tmpmount}/.snapshots"
 
-  mkdir -p "${tmpmount}/etc/nixos"
-  find ./nixos -type f -name '*.nix' -exec cp -t "${tmpmount}/etc/nixos/" {} +
+  mkdir -p \
+    "${tmpmount}/etc/nixos" \
+    "${tmpmount}/root/.ssh"
+
+  cp /home/n8henrie/git/sd_shrink/nixpi_id_rsa.pub "${tmpmount}/root/.ssh/authorized_keys"
+  cp ./mountsubvols.sh ./make_swap.sh "${tmpmount}/root/"
+  find ./nixos -type f -name '*.nix' -not -name '*-sample.nix' -exec cp -t "${tmpmount}/etc/nixos/" {} +
 }
 main "$@"
