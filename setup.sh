3aa9835971b93a1308fffb4d2797248b99a7d705
Nathan Henrie <nate@n8henrie.com>
2022-02-13 20:30:23 -0700

    Add more general setup beyond making swap

--- setup.sh
+++ setup.sh
@@ -3,17 +3,30 @@
 set -Eeuf -o pipefail
 set -x
 
-main() {
+make_swap() {
   umount -R /mnt/swap || true
   mkdir -p /mnt/swap
   mount -o subvol=@swap /dev/mmcblk0p2 /mnt/swap
-  cd /mnt/swap
+  pushd /mnt/swap
   touch ./swapfile
   chattr +C ./swapfile
   btrfs property set ./swapfile compression none
   dd if=/dev/zero of=./swapfile bs=1M count=1024 status=progress conv=fsync
   chmod 0600 ./swapfile
   mkswap ./swapfile
-  swapon ./swapfile
+  popd
+  umount -R /mnt/swap
+  rm -r /mnt/swap
 }
+
+install() {
+  nix-channel --update
+  nixos-install --root /
+}
+
+main() {
+  make_swap
+  install
+}
+
 main "$@"
