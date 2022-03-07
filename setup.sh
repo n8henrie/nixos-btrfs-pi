77d9c0fc1d83a897cc5a5a87e3b8554ea5c92274
Nathan Henrie <nate@n8henrie.com>
2022-03-07 15:08:36 -0700

    Enable swap if it's there

--- setup.sh
+++ setup.sh
@@ -4,25 +4,26 @@ set -Eeuf -o pipefail
 set -x
 
 make_swap() {
+  local swapfile=/mnt/swap/swapfile
   umount -R /mnt/swap || true
   mkdir -p /mnt/swap
   mount -o subvol=@swap /dev/mmcblk0p2 /mnt/swap
 
-  [[ -e /mnt/swap/swapfile ]] && {
+  [[ -e "${swapfile}" ]] && {
+    swapon "${swapfile}"
     umount -R /mnt/swap
     return
   }
 
   pushd /mnt/swap
-  touch ./swapfile
-  chattr +C ./swapfile
-  btrfs property set ./swapfile compression none
-  dd if=/dev/zero of=./swapfile bs=1M count=1024 status=progress conv=fsync
-  chmod 0600 ./swapfile
-  mkswap ./swapfile
+  touch "${swapfile}"
+  chattr +C "${swapfile}"
+  btrfs property set "${swapfile}" compression none
+  dd if=/dev/zero of="${swapfile}" bs=1M count=1024 status=progress conv=fsync
+  chmod 0600 "${swapfile}"
+  mkswap "${swapfile}"
+  swapon "${swapfile}"
   popd
-  umount -R /mnt/swap
-  rm -r /mnt/swap
 }
 
 install() {
