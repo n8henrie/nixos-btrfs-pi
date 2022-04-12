d0356b05d4b77c265d7b85a27340dc190646e9f7
Nathan Henrie <nate@n8henrie.com>
2022-04-12 16:44:12 -0600

    Conditionally mount swap

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -79,6 +79,12 @@
       };
     };
 
+  swapDevices = lib.mkIf (builtins.pathExists "/swap/swapfile") [
+    {
+      device = "/swap/swapfile";
+    }
+  ];
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
