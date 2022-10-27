c632614527843444231146d481227de310fb255a
Nathan Henrie <nate@n8henrie.com>
2022-10-27 13:45:55 -0600

    Flake!

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,12 +1,15 @@
 {
   description = "RPi3 on BTRFS root";
   inputs.nixpkgs.url = "nixpkgs/nixos-22.05-aarch64";
-  outputs = { self, nixpkgs }@attrs:
+  outputs =
+    { self
+    , nixpkgs
+    } @ attrs:
     let
       system = "aarch64-linux";
     in
     {
-      nixosConfigurations.machine = nixpkgs.lib.nixosSystem
+      nixosConfigurations.nixpi = nixpkgs.lib.nixosSystem
         {
           inherit system;
           specialArgs = attrs;
